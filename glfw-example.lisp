9dea8700cded15fd7d978c886e6c90891897d7b5
Kayomarz Gazder <kayomarz.gazder@ahrefs.com>
2023-04-14 11:01:25 +0530

    playing around

--- glfw-example.lisp
+++ glfw-example.lisp
@@ -1,32 +1,15 @@
 ;;;; basic-window.lisp
 ;;;; OpenGL example code borrowed from cl-opengl
-(in-package #:cl-glfw3-examples)
+;;(in-package #:cl-glfw3-examples)
+(ql:quickload '(:cl-glfw3 :cl-opengl :trivial-main-thread))
+(in-package #:cl-user)
 
-(export '(basic-window-example))
+(glfw:initialize)
 
-(def-key-callback quit-on-escape (window key scancode action mod-keys)
-  (declare (ignore window scancode mod-keys))
-  (when (and (eq key :escape) (eq action :press))
-    (set-window-should-close)))
-
-(defun render ()
-  (gl:clear :color-buffer)
-  (gl:with-pushed-matrix
-    (gl:color 1.0 1.0 1.0)
-    (gl:line-width 3.0)
-    (gl:begin :line-loop)
-    (gl:vertex 25 25 0.0)
-    (gl:vertex 25 -25 0.0)
-    (gl:vertex -25 -25 0.0)
-    (gl:vertex -25 25 0.0)
-    (gl:end)
-    ;; (gl:color 1 1 1)
-    ;; (gl:rect -25 -25 25 25)
-    )
-  )
-
-
-
+(glfw:init-2)
+(glfw:mk-window-2 (:title "Window test" :width 600 :height 400))
+(glfw:create-window :title "Window test" :width 600 :height 400)
+(gl:clear-color 1.0 0 0 0)
 (defun set-viewport (width height)
   (gl:viewport 0 0 width height)
   (gl:matrix-mode :projection)
@@ -34,33 +17,119 @@
   (gl:ortho -50 50 -50 50 -1 1)
   (gl:matrix-mode :modelview)
   (gl:load-identity))
+(set-viewport 600 400)
 
-(def-window-size-callback update-viewport (window w h)
-  (declare (ignore window))
-  (set-viewport w h))
 
-(defun basic-window-example ()
-  ;; Graphics calls on OS X must occur in the main thread
-  (with-body-in-main-thread ()
-    (with-init-window (:title "Window test" :width 600 :height 400)
-      (setf %gl:*gl-get-proc-address* #'get-proc-address)
-      (set-key-callback 'quit-on-escape)
-      (set-window-size-callback 'update-viewport)
-      (gl:clear-color 0 0 0 0)
-      (set-viewport 600 400)
-      (loop until (window-should-close-p)
-            do (render)
-            do (swap-buffers)
-            do (poll-events)))))
 
-(defun draw-square ()
-  (gl:color 1.0 1.0 1.0)
-  (gl:line-width 3.0)
-  (gl:begin :line-loop)
-  (gl:vertex 25 25 0.0)
-  (gl:vertex 25 -25 0.0)
-  (gl:vertex -25 -25 0.0)
-  (gl:vertex -25 25 0.0)
-  (gl:end))
-;;(draw-square)
-(basic-window-example)
+(unwind-protect
+     (progn
+       (glfw:with-init
+         )
+       )
+  )
+
+
+(defmacro with-redraw (&body body)
+  `(progn
+     (gl:clear :color-buffer)
+     (gl:with-pushed-matrix
+       (gl:color 1.0 1.0 1.0)
+       (gl:line-width 1.0)
+       ,@body
+       )
+     (glfw:swap-buffers)
+     ))
+
+(glfw:with-init-2
+  (glfw:mk-window-2 (:title "Window test" :width 600 :height 400))
+  ;; (setf %gl:*gl-get-proc-address* #'get-proc-address)
+  ;; (set-key-callback 'quit-on-escape)
+  ;; (set-window-size-callback 'update-viewport)
+  (gl:clear-color 1 0 0 0)
+  (set-viewport 600 400)
+  (glfw:with-context
+      (with-redraw
+        (gl:begin :line-loop)
+        (gl:vertex 25 25 0.0)
+        (gl:vertex 25 -25 0.0)
+        (gl:vertex -25 -25 0.0)
+        (gl:vertex -25 25 0.0)
+        (gl:end)
+        ))
+  )
+
+
+(glfw:with-init-window-2 (:title "Window test" :width 600 :height 400)
+  ;; (setf %gl:*gl-get-proc-address* #'get-proc-address)
+  ;; (set-key-callback 'quit-on-escape)
+  ;; (set-window-size-callback 'update-viewport)
+  (gl:clear-color 1 0 0 0)
+  (set-viewport 1600 400)
+  (with-redraw
+    (gl:begin :line-loop)
+    (gl:vertex 25 25 0.0)
+    (gl:vertex 25 -25 0.0)
+    (gl:vertex -25 -25 0.0)
+    (gl:vertex -25 25 0.0)
+    (gl:end)
+    )
+  )
+
+
+
+(glfw:with-init-window-2 (:title "Window test" :width 600 :height 400)
+  ;; (setf %gl:*gl-get-proc-address* #'get-proc-address)
+  ;; (set-key-callback 'quit-on-escape)
+  ;; (set-window-size-callback 'update-viewport)
+  (gl:clear-color 1 0 0 0)
+  (set-viewport 600 400)
+  (with-redraw
+    (in-package #:glfw)
+
+    (gl:begin :line-loop)
+    (gl:vertex 25 25 0.0)
+    (gl:vertex 125 -25 0.0)
+    (gl:vertex -25 -25 0.0)
+    (gl:vertex -25 25 0.0)
+    (gl:end)
+    (in-package #:cl-user)
+    
+    )
+  )
+
+
+;; (def-window-size-callback update-viewport (window w h)
+;;   (declare (ignore window))
+;;   (set-viewport w h))
+
+;; (defun basic-window-example ()
+;;   ;; Graphics calls on OS X must occur in the main thread
+;;   (trivial-main-thread:with-body-in-main-thread ()
+;;     (glfw:with-init-window (:title "Window test" :width 600 :height 400)
+;;       ;; (setf %gl:*gl-get-proc-address* #'get-proc-address)
+;;       ;; (set-key-callback 'quit-on-escape)
+;;       ;; (set-window-size-callback 'update-viewport)
+;;       (gl:clear-color 0 0 0 0)
+;;       (set-viewport 600 400)
+;;       (loop until ()
+;;             do (render)
+;;             do (glfw:swap-buffers)
+;;             do (glfw:poll-events)
+;;             ))
+;;     ))
+
+;; (glfw:with-init-window (:title "Window test" :width 600 :height 400)
+;;   (print "yo")
+;;   )
+
+;; (defun draw-square ()
+;;   (gl:color 1.0 1.0 1.0)
+;;   (gl:line-width 3.0)
+;;   (gl:begin :line-loop)
+;;   (gl:vertex 25 25 0.0)
+;;   (gl:vertex 25 -25 0.0)
+;;   (gl:vertex -25 -25 0.0)
+;;   (gl:vertex -25 25 0.0)
+;;   (gl:end))
+;; ;;(draw-square)
+;; (basic-window-example)
